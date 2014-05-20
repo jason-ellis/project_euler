d485a282806b4bfc8bdb92c12b5725f569fc4e29
Jason Ellis <je7979@gmail.com>
2014-05-19 20:23:04 -0500

    square root added to problem 3 solution

--- problem3.js
+++ problem3.js
@@ -1,9 +1,13 @@
 /**
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
+
+This solution is not optimized for the number given. It will take a
+very LONG time to run on that number, but it otherwise works.
 **/
 
 var subject = 13195;
+var square = Math.round(Math.sqrt(subject));
 var factors = [];
 var largest = 0;
 
@@ -21,10 +25,9 @@ for(var i = 0; i < factors.length; i++) {
     }
 }
 
+console.log(square);
 console.log("Factors of " + subject+ " are " + factors);
 
 factors.sort(function(a, b){return a-b;});
-
-console.log(factors);
 
 console.log("Largest prime factor is " + factors[factors.length-1]);
